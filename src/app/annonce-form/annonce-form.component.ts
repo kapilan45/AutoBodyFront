6b4b01596b47836cc0047ac5d2d43a8841883bd0
Kapilan <mahenthirarajahk@gmail.com>
2020-04-02 14:36:04 +0200

    commit pour demo

---
+++ src/app/annonce-form/annonce-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-annonce-form',
+  templateUrl: './annonce-form.component.html',
+  styleUrls: ['./annonce-form.component.scss']
+})
+export class AnnonceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
