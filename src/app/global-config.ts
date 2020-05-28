db5fe12a6dd2d4d64802ee0839a8adca3483663c
Mxnkey <ndezerce@gmail.com>
2020-05-28 12:24:33 +0200

    global config register NDE

--- src/app/global-config.ts
+++ src/app/global-config.ts
@@ -6,8 +6,8 @@ export class GlobalConfig {
   public static apiUrl = 'http://loacalhost:8080/api';
   public static getAnnoncesApiUrl = 'http://localhost:8080/final/annonce';
   public static getUserAnnoncesApiUrl = 'http://localhost:8080/api/test/annonce/user';
-  public static registerApiUrl = 'http://localhost:8080/api/control/register';
-  public static loginApiUrl = 'http://localhost:8080/api/test/login';
+  public static registerApiUrl = 'http://localhost:8080/users/create';
+  public static loginApiUrl = 'http://localhost:8080/login';
   public static saveAnnonceApiUrl = 'http://loacalhost:8080/api';
   public static supprimerAnnonceApiUrl = 'http://loacalhost:8080/api';
   public static modifyAnnonceApiUrl = 'http://loacalhost:8080/api';
